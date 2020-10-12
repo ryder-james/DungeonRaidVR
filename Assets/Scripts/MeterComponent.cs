1a442840e340bd3366a8b7cf1ea6121974651bcb
Ryder James <stidwell@student.neumont.edu>
2020-10-12 13:48:00 -0600

    Add MeterComponent base

---
+++ Assets/Scripts/MeterComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public abstract class MeterComponent : MonoBehaviour {
+	[SerializeField] protected float maxValue = 100;
+
+	[Tooltip("Recharge rate, in units per second")]
+	[SerializeField] protected float rechargeRate = 0;
+
+	public bool IsRecharging { get; set; } = true;
+
+	private float value;
+
+	public float Value {
+		get => value;
+		set { this.value = Mathf.Clamp(value, 0, maxValue); }
+	}
+
+	public abstract Color MeterColor { get; }
+
+	private void Awake() {
+		Value = maxValue;
+	}
+}
