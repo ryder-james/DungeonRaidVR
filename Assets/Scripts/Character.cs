db54c6417115da546e3ec011d38e9899e3100958
Ryder James <stidwell@student.neumont.edu>
2020-10-12 13:58:28 -0600

    Add Character base class

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+	[SerializeField] protected GameObject modelDataPrefab = null;
+	[Space]
+	[SerializeField] protected float hpMod = 1;
+	[SerializeField] protected MeterComponent[] meters = null;
+	[SerializeField] protected AmmoPool[] ammoPools = null;
+
+	protected virtual void Start() {
+		foreach (MeterComponent meter in meters) {
+			if (meter.GetType() == typeof(HealthComponent)) {
+				meter.MaxValue = CalculateHealth(GameObject.FindGameObjectsWithTag("Hero").Count());
+				meter.Value = meter.MaxValue;
+			}
+		}
+	}
+
+	protected virtual void Update() {
+
+	}
+
+	protected abstract float CalculateHealth(int heroCount);
+}
---
+++ Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class HealthComponent : MeterComponent {
+	public override Color MeterColor => Color.red;
+}
