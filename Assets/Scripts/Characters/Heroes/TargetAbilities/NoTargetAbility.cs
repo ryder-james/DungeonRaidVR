b89f001f11107ce3cb5f362048bfde8d0760d453
Ryder James <stidwell@student.neumont.edu>
2020-10-14 18:13:19 -0600

    Add NoTargetAbility skeleton

---
+++ Assets/Scripts/Characters/Heroes/TargetAbilities/NoTargetAbility.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NoTarget", menuName = AbilityMenuPrefix + "No Target")]
+public class NoTargetAbility : Ability {
+	protected override void TargetCast() {
+		throw new System.NotImplementedException();
+	}
+}
