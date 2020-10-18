fc7d7137f0aa798b26010b9730e5af7912fffa5c
Ryder James <stidwell@student.neumont.edu>
2020-10-18 15:57:28 -0600

    Add Hero HUD code

---
+++ Assets/Scripts/UI/HeroHUDManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DungeonRaid.Characters.Heroes;
+using DungeonRaid.Collections;
+
+public class HeroHUDManager : MonoBehaviour {
+	[SerializeField] private HeroHUD[] heroHUDs = null;
+
+	private int heroCount = 0;
+
+	public void AddHero(Hero hero) {
+		heroHUDs[heroCount].SetHero(hero);
+
+		heroCount++;
+	}
+}
---
+++ Assets/Scripts/UI/MeterUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using DungeonRaid.Collections;
+
+public class MeterUI : MonoBehaviour {
+	[SerializeField] private Slider slider = null;
+
+	public MeterComponent Meter { get; set; }
+
+	private void Awake() {
+		Meter.OnValueChanged += UpdateSlider;
+	}
+
+	private void Start() {
+		slider.fillRect.GetComponent<Image>().color = Meter.Color;
+	}
+
+	public void UpdateSlider() {
+		slider.value = Meter.Value / Meter.MaxValue;
+	}
+}
