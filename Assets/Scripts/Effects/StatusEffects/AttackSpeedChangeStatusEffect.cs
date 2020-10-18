93a64a84a62edf61e68dd3bd2ed71f8023d3540f
Ryder James <stidwell@student.neumont.edu>
2020-10-18 12:29:59 -0600

    Add Attack Speed and speed/attack speed status effects

---
+++ Assets/Scripts/Effects/StatusEffects/AttackSpeedChangeStatusEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using DungeonRaid.Characters;
+using DungeonRaid.Characters.Heroes;
+
+namespace DungeonRaid.Effects.StatusEffects {
+	
+	[CreateAssetMenu(fileName = "StatChangeEffect", menuName = StatusEffectMenuPrefix + "Stat Change")]
+	public class AttackSpeedChangeStatusEffect : StatusEffect {
+		[SerializeField] private float amount = 0;
+
+		protected override void StartEffect(Character target) {
+			(target as Hero).AttackSpeed += amount;
+		}
+
+		protected override void StopEffect(Character target) {
+			(target as Hero).AttackSpeed -= amount;
+		}
+	}
+}
---
+++ Assets/Scripts/Effects/StatusEffects/SpeedChangeStatusEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+using DungeonRaid.Characters;
+using DungeonRaid.Characters.Heroes;
+
+namespace DungeonRaid.Effects.StatusEffects {
+	
+	[CreateAssetMenu(fileName = "SpeedChange", menuName = StatusEffectMenuPrefix + "Speed Change")]
+	public class SpeedChangeStatusEffect : StatusEffect {
+		[SerializeField] private float amount = 0;
+
+		protected override void StartEffect(Character target) {
+			(target as Hero).Speed += amount;
+		}
+
+		protected override void StopEffect(Character target) {
+			(target as Hero).Speed -= amount;
+		}
+	}
+}
