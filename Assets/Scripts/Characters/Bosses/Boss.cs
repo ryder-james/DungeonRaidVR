093db91c4fb27079d3c3c5b6fc2dcab264e7f87e
Ryder James <stidwell@student.neumont.edu>
2020-10-20 14:23:01 -0600

    Add boss health meter

---
+++ Assets/Scripts/Characters/Bosses/Boss.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+using DungeonRaid.Collections;
+
+namespace DungeonRaid.Characters.Bosses {
+    public class Boss : Character {
+		[SerializeField] private float initialHealth = 100;
+
+		public float InitialHealth { get => initialHealth; set => initialHealth = value; }
+
+		protected override float CalculateHealth(int heroCount) {
+			float health = InitialHealth * Mathf.Pow(1 + hpMod, heroCount - 1);
+			return Mathf.Ceil(health);
+		}
+    }
+}
+
+
