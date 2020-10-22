cc348f7449e0e1b76b2aff6374111c89c1af692c
Ryder James <stidwell@student.neumont.edu>
2020-10-21 19:30:18 -0600

    Add projectile targeting

---
+++ Assets/Scripts/Abilities/TargetAbilities/ProjectileAbility.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+using DungeonRaid.Characters.Heroes;
+
+namespace DungeonRaid.Abilities {
+	[CreateAssetMenu(fileName = "Projectile", menuName = AbilityMenuPrefix + "Projectile")]
+	public class ProjectileAbility : Ability {
+		[SerializeField] private GameObject projectilePrefab = null;
+
+		protected override bool TargetCast() {
+			Vector3 start = Owner.Nozzle;
+			Vector3 target = (Owner as Hero).TargetPoint;
+			Vector3 dir = target - start;
+
+			float height = dir.y;
+			dir.y = 0;
+
+			float distance = dir.magnitude;
+			dir.y = distance;
+			distance += height;
+
+			float velocity = Mathf.Sqrt(distance * Physics.gravity.magnitude);
+
+			GameObject projObj = Instantiate(projectilePrefab, Owner.Nozzle, Quaternion.identity);
+			Projectile proj = projObj.GetComponent<Projectile>();
+			proj.GetComponent<Rigidbody>().AddForce(velocity * dir.normalized, ForceMode.VelocityChange);
+			proj.Owner = Owner;
+			proj.Effects = Effects;
+			return true;
+		}
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+using DungeonRaid.Abilities.Effects;
+using DungeonRaid.Characters;
+
+namespace DungeonRaid.Abilities {
+	[RequireComponent(typeof(Collider), typeof(Rigidbody))]
+	public class Projectile : MonoBehaviour {
+		[System.Flags]
+		public enum ProjectileTargetType {
+			Hero = 1 << 0,
+			Boss = 1 << 1
+		}
+
+		[SerializeField] private bool useTriggerCollision = true;
+		[SerializeField] private ProjectileTargetType targetType = ProjectileTargetType.Boss;
+
+		public Effect[] Effects { get; set; }
+		public Character Owner { get; set; }
+
+		private void ApplyAll(Character target) {
+			foreach (Effect e in Effects) {
+				e.Apply(target);
+			}
+		}
+
+		private void Hit(GameObject other) {
+			if ((other.CompareTag("Hero") && targetType.HasFlag(ProjectileTargetType.Hero)) ||
+				(other.CompareTag("Boss") && targetType.HasFlag(ProjectileTargetType.Boss))) {
+				ApplyAll(other.GetComponent<Character>());
+			}
+		}
+
+		private void OnTriggerEnter(Collider other) {
+			if (other.gameObject == Owner.gameObject) {
+				return;
+			}
+
+			if (useTriggerCollision) {
+				Hit(other.gameObject);
+				Destroy(gameObject);
+			}
+
+		}
+
+		private void OnCollisionEnter(Collision collision) {
+			Collider other = collision.collider;
+			if (other.gameObject == Owner.gameObject) {
+				return;
+			}
+
+			if (!useTriggerCollision) {
+				Hit(other.gameObject);
+				Destroy(gameObject);
+			}
+
+		}
+	}
+}
