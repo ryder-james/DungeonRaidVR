34ba106eb37274100ea36f69d3283d89aa348141
Ryder James <stidwell@student.neumont.edu>
2020-11-08 15:09:27 -0700

    Add RotateOverTime script

---
+++ Assets/Scripts/Common/RotateOverTime.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Common.Movement {
+	public class RotateOverTime : MonoBehaviour {
+		[SerializeField] private Vector3 rotationAxis = Vector3.up;
+		[SerializeField] private float rotationSpeed = 2;
+
+		private void Start() {
+			rotationAxis.Normalize();
+		}
+
+		private void Update() {
+			transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
+		}
+	}
+}
