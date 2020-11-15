671c1491e75c160588530f980164d2dacb25c8d7
Ryder James <stidwell@student.neumont.edu>
2020-11-15 13:29:59 -0700

    Add TriggerVolume base class

---
+++ Assets/Scripts/Characters/Bosses/Interactables/TriggerButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerButton : TriggerVolume {
+	[SerializeField] private float pressSpeed = 2;
+	[SerializeField] private Vector3 pressOffset = Vector3.down;
+	[SerializeField] private TriggeredBehaviour[] behaviours = null;
+
+	private bool inMotion = false;
+
+	protected override void TriggerEnter() {
+		StartCoroutine(nameof(Press));
+
+		foreach (TriggeredBehaviour behaviour in behaviours) {
+			behaviour.Trigger();
+		}
+	}
+
+	protected override void TriggerStay() {
+
+	}
+
+	protected override void TriggerExit() {
+		StartCoroutine(nameof(Release));
+	}
+
+	private IEnumerator Press() {
+		yield return new WaitUntil(() => !inMotion);
+
+		Vector3 start = transform.position;
+		Vector3 end = transform.position + pressOffset;
+
+		inMotion = true;
+		for (float t = 0; t < 1; t += Time.deltaTime * pressSpeed) {
+			transform.position = Vector3.Lerp(start, end, t);
+			yield return new WaitForEndOfFrame();
+		}
+		inMotion = false;
+
+		transform.position = end;
+	}
+
+	private IEnumerator Release() {
+		yield return new WaitUntil(() => !inMotion);
+
+		Vector3 start = transform.position;
+		Vector3 end = transform.position - pressOffset;
+
+		inMotion = true;
+		for (float t = 0; t < 1; t += Time.deltaTime * pressSpeed) {
+			transform.position = Vector3.Lerp(start, end, t);
+			yield return new WaitForEndOfFrame();
+		}
+		inMotion = false;
+
+		transform.position = end;
+	}
+}
---
+++ Assets/Scripts/Characters/Bosses/Interactables/TriggerVolume.cs
@@ -0,0 +1,63 @@
+﻿using DungeonRaid.Characters.Bosses.Interactables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TriggerVolume : Interactable {
+	[SerializeField] protected string triggerTag = "Boss";
+
+	private const float fixedChannelTimer = 1;
+
+	protected GameObject triggeringObject;
+
+	private float channelTimer = 0;
+	private bool held = false;
+
+	protected abstract void TriggerEnter();
+	protected abstract void TriggerStay();
+	protected abstract void TriggerExit();
+
+	protected void ReleaseEarly() {
+		held = false;
+		Debug.Log("trigger exit");
+		TriggerExit();
+		triggeringObject = null;
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log("object enter");
+
+		if (triggeringObject == null && other.CompareTag(triggerTag)) {
+			triggeringObject = other.gameObject;
+			channelTimer = 0;
+			held = true;
+
+			Debug.Log("trigger");
+			TriggerEnter();
+		}
+	}
+
+	private void OnTriggerStay(Collider other) {
+		Debug.Log("object stay");
+
+		if (held && other.gameObject == triggeringObject) {
+			channelTimer += Time.deltaTime;
+			if (channelTimer >= fixedChannelTimer) {
+				Debug.Log("trigger stay");
+				TriggerStay();
+				channelTimer -= fixedChannelTimer;
+			}
+
+		} else if (!held && other == triggeringObject) {
+			OnTriggerExit(other);
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		Debug.Log("object exit");
+
+		if (other.gameObject == triggeringObject) {
+			ReleaseEarly();
+		}
+	}
+}
