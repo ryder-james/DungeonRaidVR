b1cd3fc0b3aae77680a1828a8dda6c19d7d879c1
Ryder James <stidwell@student.neumont.edu>
2020-11-15 14:27:36 -0700

    Update TriggerButton for more extensibility

--- Assets/Scripts/Characters/Bosses/Interactables/TriggerButton.cs
+++ Assets/Scripts/Characters/Bosses/Interactables/TriggerButton.cs
@@ -5,57 +5,107 @@ using UnityEngine;
 
 public class TriggerButton : TriggerVolume {
 	[SerializeField] private float pressSpeed = 2;
-	[SerializeField] private bool useContinuousBehaviour = false;
-	[SerializeField, ShowIf("useContinuousBehaviour"), Indent] private bool waitForBehaviourToEnd = false;
 	[SerializeField] private Vector3 pressOffset = Vector3.down;
-	[SerializeField, HideIf("useContinuousBehaviour")] private TriggeredBehaviour behaviour = null;
-	[SerializeField, ShowIf("useContinuousBehaviour")] private ContinuousBehaviour continuousBehaviour = null;
+
+	[SerializeField] private bool limitPressability = false;
+	[SerializeField, ShowIf("limitPressability")] private ContinuousBehaviour pressLimitingBehaviour = null;
+	[SerializeField, ShowIf("limitPressability")] private bool hasTriggeredBehaviours = true;
+	[SerializeField, ShowIf("UseTriggeredBehaviours"), Indent] private TriggeredBehaviour[] triggeredBehaviours = null;
+	[SerializeField] private bool hasChanneledBehaviours = false;
+	[SerializeField, ShowIf("hasChanneledBehaviours"), Indent] private ContinuousBehaviour[] channeledBehaviours = null;
+
+	private bool UseTriggeredBehaviours {
+		get {
+			if (!limitPressability) {
+				return true;
+			} else {
+				return hasTriggeredBehaviours;
+			}
+		}
+	}
 
 	private bool inMotion = false;
-	private bool isPressed = false;
+	private bool isBeingHeld = false;
+	private bool allowManualRelease = true;
 
 	private void Start() {
-		if (!useContinuousBehaviour) {
-			waitForBehaviourToEnd = false;
-		}
-
-		if (waitForBehaviourToEnd) {
-			continuousBehaviour.OnBehaviourEnd += End;
+		if (limitPressability) {
+			pressLimitingBehaviour.OnBehaviourEnd += End;
 		}
 	}
 
 	protected override void TriggerEnter() {
-		if (!isPressed) {
-			isPressed = true;
-			StartCoroutine(nameof(Press));
-			if(useContinuousBehaviour) {
-				continuousBehaviour.Trigger();
-			} else {
-				behaviour.Trigger();
+		if (!isBeingHeld) {
+			isBeingHeld = true;
+			StartCoroutine(nameof(ChangeState), false);
+
+			if(limitPressability) {
+				pressLimitingBehaviour.Trigger();
+				allowManualRelease = false;
+			}
+
+			if (UseTriggeredBehaviours) {
+				foreach (TriggeredBehaviour behaviour in triggeredBehaviours) {
+					if (behaviour != pressLimitingBehaviour) {
+						behaviour.Trigger();
+					}
+				}
+			}
+
+			if (hasChanneledBehaviours) {
+				foreach (ContinuousBehaviour behaviour in channeledBehaviours) {
+					if (behaviour != pressLimitingBehaviour) {
+						behaviour.Trigger();
+					}
+				}
 			}
 		}
 	}
 
 	protected override void TriggerStay() {
-
+		if (hasChanneledBehaviours) {
+			foreach (ContinuousBehaviour behaviour in channeledBehaviours) {
+				if (behaviour != pressLimitingBehaviour) {
+					behaviour.UpdateBehaviour();
+				}
+			}
+		}
 	}
 
 	protected override void TriggerExit() {
-		if (isPressed && !waitForBehaviourToEnd) {
-			End();
+		if (isBeingHeld) {
+			isBeingHeld = false;
+
+			if (allowManualRelease) {
+				Release();
+			}
 		}
 	}
 
 	private void End() {
-		isPressed = false;
-		StartCoroutine(nameof(Release));
+		allowManualRelease = true;
+
+		if (!isBeingHeld) {
+			Release();
+		}
 	}
 
-	private IEnumerator Press() {
+	private void Release() {
+		if (hasChanneledBehaviours) {
+			foreach (ContinuousBehaviour behaviour in channeledBehaviours) {
+				if (behaviour != pressLimitingBehaviour) {
+					behaviour.EndBehaviour();
+				}
+			}
+		}
+		StartCoroutine(nameof(ChangeState), true);
+	}
+
+	private IEnumerator ChangeState(bool releasing) {
 		yield return new WaitUntil(() => !inMotion);
 
 		Vector3 start = transform.position;
-		Vector3 end = transform.position + pressOffset;
+		Vector3 end = transform.position + (releasing ? -pressOffset : pressOffset);
 
 		inMotion = true;
 		for (float t = 0; t < 1; t += Time.deltaTime * pressSpeed) {
@@ -67,19 +117,9 @@ public class TriggerButton : TriggerVolume {
 		transform.position = end;
 	}
 
-	private IEnumerator Release() {
-		yield return new WaitUntil(() => !inMotion);
-
-		Vector3 start = transform.position;
-		Vector3 end = transform.position - pressOffset;
-
-		inMotion = true;
-		for (float t = 0; t < 1; t += Time.deltaTime * pressSpeed) {
-			transform.position = Vector3.Lerp(start, end, t);
-			yield return new WaitForEndOfFrame();
+	private void OnDestroy() {
+		if (limitPressability) {
+			pressLimitingBehaviour.OnBehaviourEnd -= End;
 		}
-		inMotion = false;
-
-		transform.position = end;
 	}
 }
