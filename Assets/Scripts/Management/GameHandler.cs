26dc3acf79dfc90c75b99456398445f7cdc8f436
Ryder James <stidwell@student.neumont.edu>
2020-11-16 15:18:28 -0700

    Add VR UI interaction

--- Assets/Scripts/Management/GameHandler.cs
+++ Assets/Scripts/Management/GameHandler.cs
@@ -1,12 +1,17 @@
 ﻿using DungeonRaid.Characters.Bosses;
 using DungeonRaid.Characters.Heroes;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameHandler : Persistent {
 	[SerializeField] private StaticGameObjectArray heroPrefabs = null;
+
+	private Boss boss;
+	private Hero[] heroes;
+	private int deadHeroes = 0;
 
 	public void OnGameStart() {
 		SceneManager.LoadScene("CharacterSelect");
@@ -20,13 +25,30 @@ public class GameHandler : Persistent {
 	private IEnumerator StartGameAsync(GameStartInfo info) {
 		yield return SceneManager.LoadSceneAsync(info.bossName);
 
-		FindObjectOfType<Boss>().UpdateHealth(info.heroIndices.Length);
+		boss = FindObjectOfType<Boss>();
+		boss.UpdateHealth(info.heroIndices.Length);
+		boss.OnDeath += OnBossDeath;
+
+		heroes = new Hero[info.heroIndices.Length];
 
 		GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("Spawnpoint");
 		for (int i = 0; i < info.heroIndices.Length; i++) {
 			GameObject heroObj = Instantiate(heroPrefabs[info.heroIndices[i]], spawnPoints[i].transform.position, Quaternion.Euler(0, 180, 0));
-			heroObj.GetComponent<Hero>().UpdateHealth(info.heroIndices.Length);
+			heroes[i] = heroObj.GetComponent<Hero>();
+			heroes[i].UpdateHealth(info.heroIndices.Length);
+			heroes[i].OnDeath += OnHeroDeath;
 		}
+	}
+
+	private void OnHeroDeath() {
+		deadHeroes++;
+		if (deadHeroes >= heroes.Length) {
+			Debug.Log("boss wins");
+		}
+	}
+
+	private void OnBossDeath() {
+		Debug.Log("heroes win");
 	}
 
 	private struct GameStartInfo {
