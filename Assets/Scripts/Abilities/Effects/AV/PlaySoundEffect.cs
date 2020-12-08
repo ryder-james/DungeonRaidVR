152da611738d89df96b69782f1228ea585d1e610
Ryder James <stidwell@student.neumont.edu>
2020-12-07 18:42:59 -0700

    Add fighter sound FX

--- Assets/Scripts/Abilities/Effects/AV/PlaySoundEffect.cs
+++ Assets/Scripts/Abilities/Effects/AV/PlaySoundEffect.cs
@@ -6,11 +6,11 @@ namespace DungeonRaid.Abilities.Effects {
 	[CreateAssetMenu(fileName = "PlaySound", menuName = AVEffectMenuPrefix + "Play Sound")]
 	public class PlaySoundEffect : Effect {
 		[SerializeField] private bool playAtTarget = false;
-		[SerializeField, Range(0, 1)] private float volume = 0.25f;
+		[SerializeField, Range(0, 1)] private float volume = 1;
 		[SerializeField] private AudioClip clip = null;
 
 		public override void Apply(Character caster, Character target, Vector3 point) {
-			AudioSource.PlayClipAtPoint(clip, playAtTarget ? target.transform.position : point, volume * 3);
+			AudioSource.PlayClipAtPoint(clip, playAtTarget ? target.transform.position : point, volume * 50);
 		}
 	}
 }
